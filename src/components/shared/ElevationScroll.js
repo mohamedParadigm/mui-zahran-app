77af16e38483a532bbcad3dc1ae5c010cc0c9222
mohamedParadigm <m.mahrous@paradigmegypt.com>
2022-08-30 10:15:23 +0200

    update Header behaviour

--- src/components/shared/ElevationScroll.js
+++ src/components/shared/ElevationScroll.js
@@ -2,17 +2,27 @@
 import { cloneElement } from "react";
 // MUI
 import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from "@mui/material/Slide";
 
 const ElevationScroll = ({ children }) => {
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-  });
 
-  return cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-    position: trigger ? "fixed" : "static",
-  });
+  const trigger = useScrollTrigger();
+  // const trigger = useScrollTrigger({
+  //   disableHysteresis: true,
+  //   threshold: 0,
+  // });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {cloneElement(children, {
+        position: "sticky",
+      })}
+    </Slide>
+  );
+  // return cloneElement(children, {
+  //   elevation: trigger ? 4 : 0,
+  //   position: trigger ? "fixed" : "static",
+  // });
 };
 
 export default ElevationScroll;
